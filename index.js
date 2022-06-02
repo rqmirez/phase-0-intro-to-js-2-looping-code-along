7db52b8b21d29dfa6a5d634f5144e013d156a8aa
Judith Ramirez <rqmirez@gmail.com>
2022-06-02 15:52:52 -0400

    commit tree 97d27ef668ba6d9e4b9dc9d6619fd277ef25ddc0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+ const names = ["Guadalupe", "Ollie", "Aki"]
+
+ function writeCards(names){
+     const array = []
+     for (let i=0; i< names.length; i++){
+         array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+     }
+     return array
+ }
+
+ writeCards(names)
+
+ function countDown(numbers) {
+    let countDown = 10
+while (countDown > -1) {
+console.log(countDown--)
+} 
+}
+
